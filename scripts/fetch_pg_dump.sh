e93d4c09360b3b678e008db059e7b141ad4569c8
Brian Abelson <brianabelson@gmail.com>
2018-07-03 03:47:27 -0400

    edits

--- scripts/fetch_pg_dump.sh
+++ scripts/fetch_pg_dump.sh
@@ -3,7 +3,8 @@
 set -x
 set -e
 
-aws --profile gltd s3 cp s3://gltd/march/pg_dumps/2018-06-07/march.sql data/march.sql
+last_export_date='2018-07-03'
+aws --profile gltd s3 cp s3://gltd/march/pg_dumps/$last_export_date/march.sql data/march.sql
 psql march < data/march.sql
 rm -rf data/march.sql
 true;
